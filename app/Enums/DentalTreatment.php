e910184c858dfa172b827ff9b4e507e0fc43d393
Ozair0101 <ozairjan186@gmail.com>
2025-10-26 14:21:52 +0430

    add some changes

--- app/Enums/DentalTreatment.php
+++ app/Enums/DentalTreatment.php
@@ -61,6 +61,12 @@ enum DentalTreatment: string
         };
     }
 
+    public static function values(): array
+    {
+        return array_column(self::cases(), 'value');
+    }
+
+
     public static function options(): array
     {
         return array_map(
